9daf1ce02714f6be37282005f7b1c4ce8deaad37
Abhinav Singh <theawless@gmail.com>
2024-08-02 23:49:56 -0700

    Add files via upload

--- extensions/main.gd
+++ extensions/main.gd
@@ -7,10 +7,19 @@ func _input(event: InputEvent) -> void:
 		return
 	if not event.pressed:
 		return
-	if event.scancode == KEY_K:
-		_wave_timer.start(0.01)
-	if event.scancode == KEY_L:
-		var wave_data = ZoneService.get_wave_data(RunData.current_zone, RunData.current_wave);
-		_wave_timer.start(min(_wave_timer.time_left + 1, wave_data.wave_duration))
+	var wave_data = ZoneService.get_wave_data(RunData.current_zone, RunData.current_wave);
 	if event.scancode == KEY_J:
 		_wave_timer.start(max(_wave_timer.time_left - 1, 0))
+		_wave_manager.init(_wave_timer, wave_data)
+	if event.scancode == KEY_K:
+		_wave_timer.start(0.01)
+		_wave_manager.init(_wave_timer, wave_data)
+	if event.scancode == KEY_L:
+		_wave_timer.start(min(_wave_timer.time_left + 1, wave_data.wave_duration))
+		_wave_manager.init(_wave_timer, wave_data)
+	if event.scancode == KEY_I:
+		Engine.time_scale = max(0, Engine.time_scale - 0.1)
+	if event.scancode == KEY_O:
+		Engine.time_scale = 1.0
+	if event.scancode == KEY_P:
+		Engine.time_scale = min(10, Engine.time_scale + 0.1)
