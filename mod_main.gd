71070968ae8aa96bf5252351d7bfc072fd268865
Abhinav Singh <theawless@gmail.com>
2024-08-10 12:40:55 -0700

    Add files via upload

--- mod_main.gd
+++ mod_main.gd
@@ -2,9 +2,9 @@ extends Node
 
 
 func _init() -> void:
-	var dir = ModLoaderMod.get_unpacked_dir().plus_file("theawless-SkipWave")
+	var dir = ModLoaderMod.get_unpacked_dir().plus_file("theawless-TimeControl")
 	ModLoaderMod.install_script_extension(dir + "/extensions/main.gd")
 
 
 func _ready() -> void:
-	ModLoaderLog.info("Ready!", "theawless-SkipWave:Main")
+	ModLoaderLog.info("Ready!", "theawless-TimeControl:Main")
